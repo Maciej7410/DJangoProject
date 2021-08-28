725d21bf07e32a4e7d2c55caba43bd4ca859658f
Maciej <m_wlodek@poczta.onet.pl>
2021-08-28 10:59:22 +0200

    Initial commit

---
+++ viewer/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models import (Model, CharField, DateField, DateTimeField, # charField odpowiada varChar
+                                TextField,
+                              IntegerField, ForeignKey, DO_NOTHING, )
+
+
+# Create your models here.
+
+class Genre(Model): # Model dziedziczy po Base,
+    name = CharField(max_length=128)
+
+
+class Movie(Model):
+    title = CharField(max_length=128)
+    genre = ForeignKey(Genre, on_delete=DO_NOTHING) # kiedy usuwamy film zostawiamy gatunek
+    rating = IntegerField()
+    released = DateField()
+    description = TextField()
+    created = DateTimeField(auto_now_add=True) # automatycznie będzie wstawiany aktualny czas
