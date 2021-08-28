288322fe302595ff6f885908145409d5517503db
Maciej <m_wlodek@poczta.onet.pl>
2021-08-28 16:00:59 +0200

    Finish template for movies cz.2

--- viewer/views.py
+++ viewer/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
 
 # Create your views here.
 from viewer.models import Movie
 
 
 def movies(request):
-     return render(
+    return render(
         request, template_name='movies.html',
         context={
             'movies': Movie.objects.all()
