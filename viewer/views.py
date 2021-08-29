ee118e30ef3cbf67e02ea848951a05e165cda168
Maciej <m_wlodek@poczta.onet.pl>
2021-08-29 10:00:22 +0200

    Add class-based-view

--- viewer/views.py
+++ viewer/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
 # from django.http import HttpResponse
-
+from django.views import View
 # Create your views here.
 from viewer.models import Movie
 
-
-def movies(request):
-    return render(
-        request, template_name='movies.html',
-        context={
-            'movies': Movie.objects.all()
-        }
-    )
+class MoviesView(View):
+    def get(self, request):
+        return render(
+            request, template_name='movies.html',
+            context={
+                'movies': Movie.objects.all()
+            }
+        )
