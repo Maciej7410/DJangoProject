ae8abf1e374d05fcd8388f3643b71b6269dbd945
Maciej <m_wlodek@poczta.onet.pl>
2021-08-29 10:14:41 +0200

    Add temlateview based view

--- viewer/views.py
+++ viewer/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 # from django.http import HttpResponse
 
 # Create your views here.
 from viewer.models import Movie
 
-class MoviesView(TemplateView):
+class MoviesView(ListView):
     template_name = 'movies.html'
-    extra_context = {'movies' : Movie.objects.all() }
+    model = Movie
